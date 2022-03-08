4ca469a861002414a0fedbef0a6f05731ea117f4
CodingTomato <me@codingtomato.de>
2022-03-08 18:26:04 +0100

    delete token

--- server/config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  twitchApiClientId: process.env.TWITCH_API_CLIENT_ID,
-  twitchApiOauthToken: process.env.TWITCH_API_TOKEN,
-  twitchApiSecret: process.env.TWITCH_API_SECRET,
-};
